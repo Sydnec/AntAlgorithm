32dc693f8046a61be81412d42a12eb255bccf2ad
Sydnec <simon@SimonMacBook-Air.local>
2024-01-12 10:52:49 +0100

    Chronomètre

--- js/view.js
+++ js/view.js
@@ -1,8 +1,30 @@
-// View
-class View {
-    constructor() {
-        this.root = document.getElementById('app');
+let isRunning = false;
+let seconds = 0;
+let minutes = 0;
+let timerInterval;
+const timerElement = document.getElementById('timer');
+const toggleButton = document.getElementById('startstopButton');
+
+function updateTimer() {
+    seconds++;
+
+    const formattedTime = padNumber(minutes) + ':' + padNumber(seconds);
+    timerElement.textContent = formattedTime;
+}
+
+function padNumber(num) {
+    return num.toString().padStart(2, '0');
+}
+
+toggleButton.addEventListener('click', function () {
+    if (isRunning) {
+        clearInterval(timerInterval);
+        toggleButton.textContent = 'Start';
+    } else {
+        timerInterval = setInterval(updateTimer, 1000);
+        toggleButton.textContent = 'Stop';
     }
 
-    // View methods
-}
+    isRunning = !isRunning;
+    resetButton.disabled = isRunning;
+});

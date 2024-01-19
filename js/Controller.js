01b9f1d8a973b62eebc2ebe828cedf7e4f600eb0
Sydnec <97565235+Sydnec@users.noreply.github.com>
2024-01-19 16:43:46 +0100

    Tentative d'affichage des phéromones

--- js/Controller.js
+++ js/Controller.js
@@ -2,38 +2,53 @@ import { Model } from "./Model.js";
 import { View } from "./View.js";
 
 class Controller {
-    constructor(model, view) {
-        this.model = model;
-        this.view = view;
-		
-        this.view.toggleButton.addEventListener('click', () => this.toggle());
-        window.addEventListener(
-            'keydown',
-            (event, button = this.view.toggleButton) => {
-                if (event.key === ' ') {
-                    button.click();
-                }
-            },
-            true
-        );
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
 
-		this.view.renderMaze(this.model.myMaze);
-    }
-
-    toggle() {
-        if (this.model.isRunning) {
-            clearInterval(this.model.intervalId);
-            this.model.stopChrono();
-            this.view.updateTimer(this.model.ms);
-            this.view.displayStartButton();
-        } else {
-            this.model.startChrono();
-            this.model.intervalId = setInterval(() => {
-                this.model.tick();
-                this.view.updateTimer(this.model.ms);
-            }, 1000 / this.model.fps);
-            this.view.displayStopButton();
+    this.view.timerButton.addEventListener("click", () =>
+      this.toggleTimerButton()
+    );
+    this.view.pheromonesButton.addEventListener("click", () =>
+      this.togglePheromonesButton()
+    );
+    window.addEventListener(
+      "keydown",
+      (event, button = this.view.timerButton) => {
+        if (event.key === " ") {
+          button.click();
         }
+      },
+      true
+    );
+
+    this.view.renderMaze(this.model.myMaze);
+  }
+
+  togglePheromonesButton() {
+    if (this.view.showPheromones) {
+      this.view.pheromonesButton.style.backgroundColor = "#888888";
+    } else {
+      this.view.pheromonesButton.style.backgroundColor = "#4caf50";
     }
+    this.view.showPheromones = !this.view.showPheromones;
+    this.view.renderMaze(this.model.myMaze);
+  }
+
+  toggleTimerButton() {
+    if (this.model.isRunning) {
+      clearInterval(this.model.intervalId);
+      this.model.stopChrono();
+      this.view.updateTimer(this.model.ms);
+      this.view.displayStartButton();
+    } else {
+      this.model.startChrono();
+      this.model.intervalId = setInterval(() => {
+        this.model.tick();
+        this.view.updateTimer(this.model.ms);
+      }, 1000 / this.model.fps);
+      this.view.displayStopButton();
+    }
+  }
 }
 const app = new Controller(new Model(), new View());
--- js/View.js
+++ js/View.js
@@ -1,9 +1,13 @@
 export class View {
   constructor() {
-	this.cellSize;
-    this.toggleButton = document.getElementById("toggleButton");
+    this.cellSize;
+    this.showPheromones = false;
+    this.timerButton = document.getElementById("timerButton");
+    this.timerButton.style.backgroundColor = "#4caf50";
+    this.pheromonesButton = document.getElementById("pheromonesButton");
+    this.pheromonesButton.style.backgroundColor = "#888888";
     this.timerElement = document.getElementById("timer");
-	this.canvas = document.getElementById('gridCanvas')
+    this.canvas = document.getElementById("gridCanvas");
   }
 
   renderMaze(maze) {
@@ -11,20 +15,32 @@ export class View {
       this.canvas.width / maze.width,
       this.canvas.height / maze.height
     );
-	this.cellSize = Math.min(this.canvas.width/maze.width, this.canvas.height/maze.height)
-	const ctx = this.canvas.getContext('2d');
+    this.cellSize = Math.min(
+      this.canvas.width / maze.width,
+      this.canvas.height / maze.height
+    );
+    const ctx = this.canvas.getContext("2d");
 
-	for (let x = 0; x < maze.height; x++) {
-		for (let y = 0; y < maze.width; y++) {
-			const cellType = maze.cells[x][y].getType();
-			const img = new Image();
-			img.src = `images/${cellType.toLowerCase()}.png`;
+    for (let x = 0; x < maze.height; x++) {
+      for (let y = 0; y < maze.width; y++) {
+        const cellType = maze.cells[x][y].getType();
+        const img = new Image();
+        img.src = `images/${cellType.toLowerCase()}.png`;
+        if (cellType === "Free") {
+          img.textContent = maze.cells[x][y].getQty();
+        }
 
-			img.onload = () => {
-				ctx.drawImage(img, y * this.cellSize, x * this.cellSize, this.cellSize, this.cellSize);
-			};
-		}
-	}
+        img.onload = () => {
+          ctx.drawImage(
+            img,
+            y * this.cellSize,
+            x * this.cellSize,
+            this.cellSize,
+            this.cellSize
+          );
+        };
+      }
+    }
   }
 
   updateTimer(ms) {
@@ -43,11 +59,11 @@ export class View {
   }
 
   displayStartButton() {
-    this.toggleButton.textContent = "Start";
-    this.toggleButton.style.backgroundColor = "#4caf50";
+    this.timerButton.textContent = "Start";
+    this.timerButton.style.backgroundColor = "#4caf50";
   }
   displayStopButton() {
-    this.toggleButton.textContent = "Stop";
-    this.toggleButton.style.backgroundColor = "#d70000";
+    this.timerButton.textContent = "Stop";
+    this.timerButton.style.backgroundColor = "#d70000";
   }
 }
